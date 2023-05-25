7eefe843d8ce6f6ce947eeb5687cd0515dd31d13
joao-marques-a22108693 <117601737+joao-marques-a22108693@users.noreply.github.com>
2023-05-25 09:38:28 +0100

    Commit

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Area
+
+
 # Create your views here.
 
 def index_view(request):
-    return render(request, 'blog/index.html')
+    areas = Area.objects.all()
+    return render(request, 'blog/index.html', context={'areas': areas})
